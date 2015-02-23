bf6a4eb16cd4d82e68a9901b0e156f119b2b9a4d
applecool <kmitharsha@gmail.com>
2015-02-22 21:13:43 -0600

    Fixed path issues

--- Twitter Post/js/app.js
+++ Twitter Post/js/app.js
@@ -2,6 +2,8 @@ var main = function() {
     $('.btn').click(function() {
          var post = $('.status-box').val();
          $('<li>').text(post).prependTo('.posts');
+         $('.status-box').val('');
+         $('.counter').text('140');
         });
         
       $('.status-box').keyup(function(){
