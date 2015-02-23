cd24ec37882f19fdaf08839ff6f4cbfaf85ad629
applecool <kmitharsha@gmail.com>
2015-02-22 21:05:49 -0600

    Added keyup event handler in JS

--- Twitter Post/js/app.js
+++ Twitter Post/js/app.js
@@ -3,6 +3,12 @@ var main = function() {
          var post = $('.status-box').val();
          $('<li>').text(post).prependTo('.posts');
         });
+        
+      $('.status-box').keyup(function(){
+         var postLength = $(this).val().length();
+         var charactersLeft = 140 - postLength;
+         $('.counter').text(charactersLeft);
+        });
 };
 
 $(document).ready(main);
