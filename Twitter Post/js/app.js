07501ac25c53831a276ac55a2f7fafa6fb9d8daa
applecool <kmitharsha@gmail.com>
2015-02-22 21:29:08 -0600

    Fixed length bug and added full functionality

--- Twitter Post/js/app.js
+++ Twitter Post/js/app.js
@@ -4,13 +4,26 @@ var main = function() {
          $('<li>').text(post).prependTo('.posts');
          $('.status-box').val('');
          $('.counter').text('140');
+         $('.btn').addClass('disabled');
         });
-        
-      $('.status-box').keyup(function(){
-         var postLength = $(this).val().length();
+    
+    $('.status-box').keyup(function(){
+         var postLength = $(this).val().length;
          var charactersLeft = 140 - postLength;
          $('.counter').text(charactersLeft);
+         
+         if(charactersLeft <0){
+          $('.btn').addClass('disabled');
+         }
+         else if(charactersLeft == 140){
+          $('.btn').addClass('disabled');
+         }
+         else {
+          $('.btn').removeClass('disabled');
+         }
         });
+        
+        $('.btn').addClass('disabled');
 };
 
 $(document).ready(main);
